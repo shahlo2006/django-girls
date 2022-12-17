526096b6478b17db6a735e26a3252add509db894
Shahlo <shahloabdullaeva2@gmail.com>
2022-12-17 15:33:26 +0500

    corrected path join

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR / 'media')   
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   
