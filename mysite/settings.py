d6dc75f0f527a87fad39728a5c969bb65e3dd4f9
Shahlo <shahloabdullaeva2@gmail.com>
2022-12-17 15:14:10 +0500

    update settings | add gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')   
